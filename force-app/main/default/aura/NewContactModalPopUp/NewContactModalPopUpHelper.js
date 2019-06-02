bc338f2e4f5a8608f317a80adb3beb0adb6a3efb
VenkatKoppolu <venkat.koppolu@bullhorn.com>
2019-06-02 22:43:23 +0530

    enchancement for footer

--- force-app/main/default/aura/NewContactModalPopUp/NewContactModalPopUpHelper.js
+++ force-app/main/default/aura/NewContactModalPopUp/NewContactModalPopUpHelper.js
@@ -1,14 +1,22 @@
 ({
 	toggleOneAndTwoSteps : function(component) {
-		var stepOne = component.find("stepOne");
+        var stepOne = component.find("stepOne");
+        var stepOneFooter = component.find("stepOneFooter");
         $A.util.toggleClass(stepOne, 'slds-hide');
+        $A.util.toggleClass(stepOneFooter, 'slds-hide');
         var stepTwo = component.find("stepTwo");
+        var stepTwoFooter = component.find("stepTwoFooter");
         $A.util.toggleClass(stepTwo, 'slds-hide');
+        $A.util.toggleClass(stepTwoFooter, 'slds-hide');
 	},
     toggleTwoAndThreeSteps : function(component){
         var stepTwo = component.find("stepTwo");
+        var stepTwoFooter = component.find("stepTwoFooter");
         $A.util.toggleClass(stepTwo, 'slds-hide');
+        $A.util.toggleClass(stepTwoFooter, 'slds-hide');
         var stepThree = component.find("stepThree");
+        var stepThreeFooter = component.find("stepThreeFooter");
         $A.util.toggleClass(stepThree, 'slds-hide');
+        $A.util.toggleClass(stepThreeFooter, 'slds-hide');
     }
 })
